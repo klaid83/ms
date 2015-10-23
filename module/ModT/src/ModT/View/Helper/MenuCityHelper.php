e3435c399b4c2a6ab5c573f57c95143c3c114b45
Mitrofanov Sergey <mitrofanov@f-technology.ru>
2015-10-23 14:35:18 +0300

    Layout template in layout

--- module/ModT/src/ModT/View/Helper/MenuCityHelper.php
+++ module/ModT/src/ModT/View/Helper/MenuCityHelper.php
@@ -6,17 +6,35 @@ use Zend\View\Helper\AbstractHelper;
 
 class MenuCityHelper extends AbstractHelper
 {
-	public function __invoke($arr)
+	public function __invoke($menu)
 	{
-		if (is_array($arr))
+		if (is_array($menu))
 		{
-//			\Zend\Debug\Debug::dump($this->getServiceLocator()->getServiceLocator());
-//			echo '<ul>';
-//			foreach($arr as $v)
-//			{
-//				echo '<li>' . $v . '</li>';
-//			}
-//			echo '</ul>';
+			?>
+			<div class="row">
+				<div class="col-md-11">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Cities</h3>
+						</div>
+						<div class="panel-body">
+							<?php
+								echo '<ul>';
+								foreach($menu as $k=>$v)
+								{
+									echo '<li>'
+										. '<a href="/city/' . $k . '" >'
+										. $v
+										. '</a>'
+										. '</li>';
+								}
+								echo '</ul>';
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php
 		}
 	}
 }
